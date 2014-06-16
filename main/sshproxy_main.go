77ef0e9439741e041f53618deb9f734afed79bc7
Thomas Habets <thomas@habets.se>
2014-06-17 00:02:35 +0200

    Add copyright stuff.

--- main/sshproxy_main.go
+++ main/sshproxy_main.go
@@ -1,5 +1,34 @@
 package main
 
+/*
+ *  Copyright (C) 2014 Thomas Habets <thomas@habets.se>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+   ssh-keygen -N "" -f id_rsa
+   ./listener -listen 127.0.0.1:2022 \
+       ./sshproxy \
+       -conn_fd '{}' \
+       -keyfile id_rsa \
+       -target 127.0.0.1:22 \
+       -logdir sshlogdir \
+       -log_upstream \
+       -log_downstream \
+       -auth=kbi
+ *
+ **/
 import (
 	"flag"
 	"fmt"
